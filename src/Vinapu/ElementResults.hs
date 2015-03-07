f394708db7f0322781c805e70fbe1257bb42dd89
rcs <rolf.c.stadheim@gmail.com>
2015-03-07 16:35:59 +0100

    HtmlRow/Table

--- src/Vinapu/ElementResults.hs
+++ src/Vinapu/ElementResults.hs
@@ -19,22 +19,51 @@ data ElementResult = ElementResult {
                 nr1, nr2 :: NodeResult
             } deriving Show
 
+
+data HtmlRow = 
+    HtmlRow {
+        titleCol :: String,
+        serviceCol :: String,
+        ultimateCol :: String
+    } 
+    | EmptyHtmlRow deriving Show
+        
+data HtmlTable =
+    HtmlTable {
+        title :: String,
+        rows :: [HtmlRow]
+    } deriving Show
+
+htmlRow2str :: HtmlRow -> String
+htmlRow2str row = undefined
+
+htmlTable2str :: HtmlRow -> String
+htmlTable2str table = undefined
+
 -- | Node sums for node n for all elements in elx 
 sumNode :: [E.Element] 
            -> N.Node 
            -> Maybe LoadSU
 sumNode elx n = let latn = E.unitLoadAtNode n in foldr (<++>) Nothing $ map latn elx
 
-loadStr :: Maybe LoadSU -> String
-loadStr load = 
+sumLoad2str :: Maybe LoadSU -> String
+sumLoad2str load = 
     case load of 
         Nothing -> "-"
         Just ld -> let sload = (LU.service ld)
                        uload = (LU.ultimate ld) in
                             printf "\t\tBruksgrense: %.2f kN/m, bruddgrense: %.2f kN/m (%.2f)" sload uload (uload / sload)
 
-loadPairStr :: Maybe L.LoadPair -> String
-loadPairStr lp = 
+sumLoad2htmlRow :: Maybe LoadSU -> HtmlRow
+sumLoad2htmlRow load =
+    case load of 
+        Nothing -> EmptyHtmlRow 
+        Just ld -> let sload = (LU.service ld)
+                       uload = (LU.ultimate ld) 
+                       outFn x = printf "%.2f kN/m" x in HtmlRow "Sum" (outFn sload) (outFn uload)
+
+loadPair2str :: Maybe L.LoadPair -> String
+loadPair2str lp = 
     case lp of 
         Nothing -> "-"
         Just lp' -> printf "\t\t%s\n\t\t%s" (printLoad (L.deadLoad lp')) (printLoad (L.liveLoad lp'))
@@ -42,9 +71,15 @@ loadPairStr lp =
                   printLoad x = let lsu = L.loadSU1 x in printf "%s: bruksgrense %.2f kN/m2, bruddgrense %.2f kN/m2" (L.desc x) (LU.service lsu) (LU.ultimate lsu)
              
 
+loadPair2htmlRow :: Maybe L.LoadPair -> HtmlRow
+loadPair2htmlRow lp = 
+    case lp of 
+        Nothing -> EmptyHtmlRow 
+        Just lp' -> HtmlRow "-" "-" "-"
+
 printSpanned :: N.Node -> [E.Element] -> IO ()
 printSpanned node spanned = 
-    let printSpan el = putStr "\t" >> putStrLn (E.fullDesc el)  >> (putStrLn . loadPairStr . (E.loadPairAtNode node)) el in
+    let printSpan el = putStr "\t" >> putStrLn (E.fullDesc el)  >> (putStrLn . loadPair2str . (E.loadPairAtNode node)) el in
         mapM_ printSpan spanned >> return ()
 
 printNodeResult :: NodeResult -> IO ()
@@ -53,7 +88,7 @@ printNodeResult NodeResult { node,spanned} =
         sumLoad = sumNode spanned node in 
     putStrLn nodeStr >>
     printSpanned node spanned >>
-    putStrLn "\tSum:" >> putStrLn (loadStr sumLoad) >> return ()
+    putStrLn "\tSum:" >> putStrLn (sumLoad2str sumLoad) >> return ()
 
 
 printElementResult :: ElementResult -> IO ()
