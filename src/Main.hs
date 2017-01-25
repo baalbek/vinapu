4373a3c7fed5ddc433d9842090802de64297c94e
rcs <rolf.c.stadheim@gmail.com>
2017-01-25 22:17:02 +0100

    Bugfix onlysystem cmd line arg

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE FlexibleInstances,MultiParamTypeClasses,DeriveDataTypeable #-}
 -- #define RCS_DEMO
 
-import qualified Text.XML.Light as X 
+import qualified Text.XML.Light as X
 import Text.Printf (printf)
 
 import System.Console.CmdArgs (cmdArgs,Data,Typeable,typ,def,groupname,(&=))
 import qualified Vinapu.System as S
 import qualified Vinapu.Printers as P
 
-data CmdLine = 
+data CmdLine =
     CmdLine {
-        isxml :: Bool 
-        ,ishtml :: Bool 
+        isxml :: Bool
+        ,ishtml :: Bool
         ,htmlpath :: String
         ,htmlname :: String
         ,xml :: String
@@ -21,7 +21,7 @@ data CmdLine =
         ,password:: String
         ,system :: Int
         ,project :: Int
-        ,onlysystem ::Bool 
+        ,onlysystem ::Bool
         ,loadcase :: Int} deriving (Show, Data, Typeable)
 
 cmdLine = CmdLine {
@@ -34,34 +34,34 @@ cmdLine = CmdLine {
         ,xml = "/home/rcs/opt/haskell/vinapu/demo/laster.xml" &= groupname "Input/output"
         ,htmlpath = "/home/rcs/opt/haskell/vinapu/demo" &= groupname "Input/output"
         ,htmlname = "laster.html" &= groupname "Input/output"
-        ,system = 1 &= groupname "System"
+        ,system = 3 &= groupname "System"
         ,project = 1 &= groupname "System"
         ,onlysystem = False &= groupname "System"
         ,loadcase = 1 &= groupname "System"
-        ,ishtml = True &= groupname "Input/output" }
+        ,ishtml = False &= groupname "Input/output" }
 
 getPrinters :: CmdLine -> IO [P.Printer]
 getPrinters opts =
-   case (ishtml opts) of  
+   case (ishtml opts) of
         True -> let htmlresult = printf "%s/%s" (htmlpath opts) (htmlname opts) in
-                    return [P.StdoutPrinter,P.HtmlPrinter htmlresult] 
+                    return [P.StdoutPrinter,P.HtmlPrinter htmlresult]
         False -> return [P.StdoutPrinter]
-    
+
 runDbSystem :: CmdLine -> IO ()
-runDbSystem opts = 
+runDbSystem opts =
     let dbHost = (host opts)
         dbName = (dbname opts)
         dbUser = (user opts)
         dbPassword = (password opts)
         onlysys = (onlysystem opts)
-        sysId = case onlysys of True -> (project opts)
-                                False -> (system opts) 
-        elementFn = case onlysys  of True -> S.elementResultsSysId 
+        sysId = case onlysys of True -> (system opts)
+                                False -> (project opts)
+        elementFn = case onlysys  of True -> S.elementResultsSysId
                                      False -> S.elementResultsProjId  in
     getPrinters opts >>= \printers ->
         putStrLn (show opts) >>
         elementFn dbHost dbName dbUser dbPassword sysId >>= \elx ->
-        S.printElementResults elx printers 
+        S.printElementResults elx printers
 
 runXmlSystem :: CmdLine -> IO ()
 runXmlSystem opts = undefined
@@ -70,18 +70,17 @@ runXmlSystem opts = undefined
     readFile (xml opts) >>= \s ->
         let lc = show (loadcase opts)
             printers = [P.StdoutPrinter] in
-            putStrLn ("\nLoad case: " ++ lc ++ "\n") >> 
+            putStrLn ("\nLoad case: " ++ lc ++ "\n") >>
             case X.parseXMLDoc s of
                 Nothing -> error "Failed to parse xml"
                 Just doc -> S.runVinapuXml doc lc printers
             >> return ()
- -}       
+ -}
 
 main :: IO ()
-main = cmdArgs cmdLine >>= \opts -> 
-    if (isxml opts) == False 
+main = cmdArgs cmdLine >>= \opts ->
+    if (isxml opts) == False
         then
             runDbSystem opts
         else
             runXmlSystem opts
-        
